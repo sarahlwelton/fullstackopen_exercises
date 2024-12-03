05f7acdd7fe69da55ea9e4beb74f5ce9188bf2ad
Sarah Welton <sarah.welton@couchbase.com>
2024-12-03 13:12:32 -0500

    Exercise 1.7: unicafe Step 2 - Show more stats (total feedback, average score, percentage of positive)

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,29 +9,43 @@ const Button = (props) => {
   )
 }
 
-
-
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [avg, setAvg] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const logGoodFeedback = () => {
-    console.log(good)
+    console.log('good', good)
     const updatedGood = good + 1
     setGood(updatedGood)
+    setTotal(updatedGood + neutral +  bad)
+    const updatedTotal = total + 1
+    setAvg((avg + 1) / updatedTotal)
+    setPositive((updatedGood / updatedTotal) * 100)
   }
 
   const logNeutralFeedback = () => {
-    console.log(neutral)
+    console.log('neutral', neutral)
     const updatedNeutral = neutral + 1
     setNeutral(updatedNeutral)
+    setTotal(updatedNeutral + good + bad)
+    const updatedTotal = total + 1
+    setAvg((avg + 0) / updatedTotal)
+    setPositive((good / updatedTotal) * 100)
   }
 
   const logBadFeedback = () => {
-    console.log(bad)
+    console.log('bad', bad)
     const updatedBad = bad + 1
     setBad(updatedBad)
+    setTotal(updatedBad + good + neutral)
+    const updatedTotal = total + 1
+    setAvg((avg - 1) / updatedTotal)
+    setPositive((good / updatedTotal) * 100)
+    
   }
 
   return (
@@ -44,6 +58,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {avg}</p>
+      <p>positive {positive}%</p>
     </div>
   )
 }
