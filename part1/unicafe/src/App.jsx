a1f62d6f6ddf418722543a507a42d28aa2ddc3ff
Sarah Welton <sarah.welton@couchbase.com>
2024-12-03 13:53:51 -0500

    Exercise 1.11: unicafe Step 6: Format Statistics as an HTML table

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -22,12 +22,16 @@ const Statistics = (props) => {
   return (
     <div>
       <h1>statistics</h1>
-      <StatisticLine text="good" value={props.good} />
-      <StatisticLine text="neutral" value={props.neutral} />
-      <StatisticLine text="bad" value={props.bad} />
-      <StatisticLine text="all" value={props.total} />
-      <StatisticLine text="average" value={props.avg} />
-      <StatisticLine text="positive" value={props.positive} />
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={props.good} />
+          <StatisticLine text="neutral" value={props.neutral} />
+          <StatisticLine text="bad" value={props.bad} />
+          <StatisticLine text="all" value={props.total} />
+          <StatisticLine text="average" value={props.avg} />
+          <StatisticLine text="positive" value={props.positive} />
+        </tbody>
+      </table>
     </div>
   )
 
@@ -37,15 +41,17 @@ const StatisticLine = (props) => {
 
   if (props.text === "positive"){
     return (
-      <div>
-        <p>{props.text} {props.value}%</p>
-      </div>
+      <tr>
+        <td>{props.text}</td> 
+        <td>{props.value}%</td>
+      </tr>
     )
   }
   return (
-    <div>
-      <p>{props.text} {props.value}</p>
-    </div>
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
   )
 }
 
