5cf8fec7cafc432e93292ddc785649cf0856af81
Sarah Welton <sarah.welton@couchbase.com>
2024-12-03 13:47:39 -0500

    Exercise 1.10: unicafe Step 5: Make a Button and StatisticLine component
    (Button component was already made, so make StatisticLine)

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -22,15 +22,31 @@ const Statistics = (props) => {
   return (
     <div>
       <h1>statistics</h1>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>all {props.total}</p>
-      <p>average {props.avg}</p>
-      <p>positive {props.positive}%</p>
+      <StatisticLine text="good" value={props.good} />
+      <StatisticLine text="neutral" value={props.neutral} />
+      <StatisticLine text="bad" value={props.bad} />
+      <StatisticLine text="all" value={props.total} />
+      <StatisticLine text="average" value={props.avg} />
+      <StatisticLine text="positive" value={props.positive} />
     </div>
   )
 
+}
+
+const StatisticLine = (props) => {
+
+  if (props.text === "positive"){
+    return (
+      <div>
+        <p>{props.text} {props.value}%</p>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <p>{props.text} {props.value}</p>
+    </div>
+  )
 }
 
 const App = () => {
