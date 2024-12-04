57191cc3f44ade289f6fd816efa95c30dd5bf977
Sarah Welton <sarah.welton@couchbase.com>
2024-12-04 12:23:23 -0500

    Exercise 1.13: anecdotes Step 2: Add voting functionality for displayed anecdotes

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -11,19 +11,41 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
+
+  // Fill the array with the correct number of 0s to start the State
+  let initialPoints = Array(anecdotes.length).fill(0, 0, anecdotes.length)
    
   const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(initialPoints)
 
   const randomAnecdote = (min, max) => {
-    console.log('min', min, 'max', max)
+    // console.log('min', min, 'max', max)
     const random = Math.floor(Math.random() * max)
     setSelected(random)
-    console.log(selected)
+    console.log('selected anecdote', selected)
+  }
+
+  const vote = (points) => {
+    // Check current value of points
+    console.log('points', points)
+    // Create new copy of points array to mutate
+    const copyPoints = [...points]
+    // Check value of new array, copyPoints
+    console.log('copyPoints before', copyPoints)
+    // Increment the anecdote's vote value that was selected by randomAnecdote()
+    copyPoints[selected] += 1
+    // Set state to new mutated array
+    setPoints(copyPoints)
+    // Log value of new array, just to be sure. 
+    console.log('copyPoints after', copyPoints)
+
   }
 
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has { points[selected] } votes</p>
+      <button onClick={() => vote(points)}>vote</button>
       <button onClick={() => randomAnecdote(0, anecdotes.length)}>next anecdote</button>
     </div>
   )
