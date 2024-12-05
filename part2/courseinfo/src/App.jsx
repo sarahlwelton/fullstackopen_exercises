bf888e66cce4be0f5ff1a6ff25acac526ca621cc
Sarah Welton <sarah.welton@couchbase.com>
2024-12-05 10:01:10 -0500

    Exercise 2.4: ccourseinfo Step 9: Allow for an arbitrary number of courses

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -3,7 +3,7 @@ const Header = ( {course} ) => {
   console.log('the course Header received is', course)
   return (
     <>
-      <h1>{course.name}</h1>
+      <h2>{course.name}</h2>
     </>
   )
 }
@@ -44,7 +44,7 @@ const Total = ( {course} ) => {
 
   return (
     <>
-      <p>Number of exercises: {total} </p>
+      <p><strong>Total exercises: {total} </strong></p>
     </>
   )
 }
@@ -63,34 +63,59 @@ const Course = ( {course} ) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Rendering a collection, modules',
-        exercises: 4,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return (
+  <div>
+    <h1>Web development curriculum </h1>
+    {courses.map(course =>
+        <Course key={course.id} course={course} />
+      )}
+  </div> 
+  )
 }
 
 export default App
