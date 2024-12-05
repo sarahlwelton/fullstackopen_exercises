00f2ebeb9ed60e20e7104db9b3e79ce18930f1b4
Sarah Welton <sarah.welton@couchbase.com>
2024-12-05 16:27:19 -0500

    Exercise 2.9 phonebook Step 4: Add a search field to search for people by name, make it case insensitive

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -8,19 +8,21 @@ const Person = ( {person} ) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '040-1234567'
-     }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length + 1
     }
     if (persons.some((person) => newName === person.name)) {
       window.alert(`${newName} is already in the phonebook.`)
@@ -30,6 +32,23 @@ const App = () => {
       setPersons(persons.concat(personObject))
       setNewName('')
       setNewNumber('')
+    }
+    
+  }
+
+  const findPerson = ({persons}, searchString) => {
+    console.log('persons is', persons)
+    console.log('personFilter is', searchString)
+    
+    const lowerCaseFilter = searchString
+    console.log(lowerCaseFilter)
+
+    if (searchString) {
+      return persons.filter(person => {
+        return person.name.toLowerCase().includes(lowerCaseFilter)
+      })
+    } else {
+      return persons
     }
     
   }
@@ -44,9 +63,23 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setFilter(event.target.value.toLowerCase())
+  }
+
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(filter)
+  )
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        search for <input 
+        onChange={handleFilterChange}/>
+      </div>
+      <h2>Add a new number</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -66,8 +99,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person =>
-          <Person key = {person.name} person={person} />
+        {filteredPersons.map(person =>
+          <Person key = {person.id} person={person} />
         )}
       </div>
     </div>
