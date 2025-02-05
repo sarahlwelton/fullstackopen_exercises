b9f9698d363db423712e42ebbe0cf63c6de100c4
Sarah Welton <sarah.welton@couchbase.com>
2025-02-05 12:24:26 -0500

    Notes from last part (for real this time) of Part 4b - refactoring tests

--- part4 - notes app/tests/note_api.test.js
+++ part4 - notes app/tests/note_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const helper = require('./test_helper')
@@ -9,8 +9,122 @@ const api = supertest(app)
 
 const Note = require('../models/note')
 
+// Let's refactor our tests to group them with describe blocks for readability
+describe('when there are some notes saved initially', () => {
+  beforeEach(async () => {
+    await Note.deleteMany({})
+    await Note.insertMany(helper.initialNotes)
+  })
+
+  test('notes are returned as json', async () => {
+    await api
+      .get('/api/notes')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all notes are returned', async () => {
+    const response = await api.get('/api/notes')
+
+    assert.strictEqual(response.body.length, helper.initialNotes.length)
+  })
+
+  test('a specific note is within the returned notes', async () => {
+    const response = await api.get('/api/notes')
+
+    const contents = response.body.map(r => r.content)
+    assert(contents.includes('Browser can execute only JavaScript'))
+  })
+
+  describe('viewing a specific note', () => {
+
+    test('succeeds with a valid id', async () => {
+      const notesAtStart = await helper.notesInDb()
+
+      const noteToView = notesAtStart[0]
+
+      const resultNote = await api
+        .get(`/api/notes/${noteToView.id}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+      assert.deepStrictEqual(resultNote.body, noteToView)
+    })
+
+    test('fails with status code 404 if note does not exist', async () => {
+      const validNonexistingId = await helper.nonExistingId()
+
+      await api
+        .get(`/api/notes/${validNonexistingId}`)
+        .expect(404)
+    })
+
+    test('fails with status code 400 id is invalid', async () => {
+      const invalidId = '5a3d5da59070081a82a3445'
+
+      await api
+        .get(`/api/notes/${invalidId}`)
+        .expect(400)
+    })
+  })
+
+  describe('addition of a new note', () => {
+    test('succeeds with valid data', async () => {
+      const newNote = {
+        content: 'async/await simplifies making async calls',
+        important: true,
+      }
+
+      await api
+        .post('/api/notes')
+        .send(newNote)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const notesAtEnd = await helper.notesInDb()
+      assert.strictEqual(notesAtEnd.length, helper.initialNotes.length + 1)
+
+      const contents = notesAtEnd.map(n => n.content)
+      assert(contents.includes('async/await simplifies making async calls'))
+    })
+
+    test('fails with status code 400 if data invalid', async () => {
+      const newNote = {
+        important: true
+      }
+
+      await api
+        .post('/api/notes')
+        .send(newNote)
+        .expect(400)
+
+      const notesAtEnd = await helper.notesInDb()
+
+      assert.strictEqual(notesAtEnd.length, helper.initialNotes.length)
+    })
+  })
+
+  describe('deletion of a note', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
+      const notesAtStart = await helper.notesInDb()
+      const noteToDelete = notesAtStart[0]
+
+      await api
+        .delete(`/api/notes/${noteToDelete.id}`)
+        .expect(204)
+
+      const notesAtEnd = await helper.notesInDb()
+
+      assert.strictEqual(notesAtEnd.length, helper.initialNotes.length - 1)
+
+      const contents = notesAtEnd.map(r => r.content)
+      assert(!contents.includes(noteToDelete.content))
+    })
+  })
+})
+
 // We will move this into test_helper.js instead.
-/* const initialNotes = [
+/*const initialNotes = [
   {
     content: 'HTML is easy',
     important: false,
@@ -19,10 +133,10 @@ const Note = require('../models/note')
     content: 'Browser can execute only JavaScript',
     important: true,
   },
-] */
+]
 
 // Before we run each test, we'll run this to clear out the database and save the two notes stored in the initialNotes array
-beforeEach(async () => {
+  beforeEach(async () => {
 
   await Note.deleteMany({})
 
@@ -52,7 +166,7 @@ beforeEach(async () => {
   // beforeEach doesn't wait for the forEach promises to finish.
   // The execution of tests begins immediately after beforeEach has finished - so
   // tests execute before the database state is initialized.
-  /* await Note.deleteMany({})
+  await Note.deleteMany({})
   console.log('cleared')
 
   helper.initialNotes.forEach(async (note) => {
@@ -60,14 +174,14 @@ beforeEach(async () => {
     await noteObject.save()
     console.log('saved')
   })
-  console.log('done') */
+  console.log('done')
 
   // This works, but there is a better way to save multiple objects to the DB.
-  /* await Note.deleteMany({})
+  await Note.deleteMany({})
   let noteObject = new Note(helper.initialNotes[0])
   await noteObject.save()
   noteObject = new Note(helper.initialNotes[1])
-  await noteObject.save() */
+  await noteObject.save()
 })
 
 test('notes are returned as json', async () => {
@@ -158,6 +272,7 @@ test('a note can be deleted', async () => {
 
   assert.strictEqual(notesAtEnd.length, helper.initialNotes.length - 1)
 })
+*/
 
 // This test makes an HTTP GET request and verifies that the response is in JSON format with response code 200
 /* test('notes are returned as json', async () => {
