255cf3f6c22e4c21c52013339abef5b56934d266
Sarah Emmett <sarah.welton@couchbase.com>
2025-09-12 16:06:19 -0400

    Notes for Part 7a

---
+++ part7-react-router-notes/src/App.jsx
@@ -0,0 +1,155 @@
+import { useState } from 'react'
+
+import {
+  Routes,
+  Route,
+  Link,
+  Navigate,
+  useParams,
+  useNavigate,
+  useMatch
+} from "react-router-dom"
+
+
+const Home = () => (
+  <div>
+    <h2>TKTL notes app</h2>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+  </div>
+)
+
+const Note = ({ note }) => {
+  // The useParams function lets us access the URL parameter (id of the note to be displayed)
+  // We use this when the props is set to { notes } - in our new implementation, we don't need this
+  //const id = useParams().id
+  //const note = notes.find(n => n.id === Number(id))
+  return (
+    <div>
+      <h2>{note.content}</h2>
+      <div>{note.user}</div>
+      <div><strong>{note.important ? 'important' : ''}</strong></div>
+    </div>
+  )
+}
+// We render Notes as a clickable Link component that takes you to the body of the note
+const Notes = ({ notes }) => (
+  <div>
+    <h2>Notes</h2>
+    <ul>
+      {notes.map(note =>
+        <li key={note.id}>
+          <Link to={`/notes/${note.id}`}>{note.content}</Link>
+        </li>
+      )}
+    </ul>
+  </div>
+)
+
+const Users = () => (
+  <div>
+    <h2>TKTL notes app</h2>
+    <ul>
+      <li>Matti Luukkainen</li>
+      <li>Juha Tauriainen</li>
+      <li>Arto Hellas</li>
+    </ul>
+  </div>
+)
+
+const Login = (props) => {
+  // We use the useNavigate function to change the browser's URL programmatically 
+  // So when the user logs in, we programmatically change to '/' and Home
+  const navigate = useNavigate()
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    props.onLogin('mluukkai')
+    navigate('/')
+  }
+
+  return (
+    <div>
+      <h2>login</h2>
+      <form onSubmit={onSubmit}>
+        <div>
+          username: <input />
+        </div>
+        <div>
+          password: <input type='password' />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+}
+
+const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      content: 'HTML is easy',
+      important: true,
+      user: 'Matti Luukkainen'
+    },
+    {
+      id: 2,
+      content: 'Browser can execute only JavaScript',
+      important: false,
+      user: 'Matti Luukkainen'
+    },
+    {
+      id: 3,
+      content: 'Most important methods of HTTP-protocol are GET and POST',
+      important: true,
+      user: 'Arto Hellas'
+    }
+  ])
+
+  const [user, setUser] = useState(null)
+
+  const login = (user) => {
+    setUser(user)
+  }
+
+  const padding = {
+    padding: 5
+  }
+  // Every time the App component is rendered, if the URL matches '/notes/:id', then "match"
+  // contains an object that lets us access the parameterized part of the path (the id of the note)
+  // and we can fetch the correct note to display.
+  const match = useMatch('/notes/:id')
+  const note = match 
+    ? notes.find(note => note.id === Number(match.params.id))
+    : null
+
+  return (
+    <div>
+        <div>
+          <Link style={padding} to="/">home</Link>
+          <Link style={padding} to="/notes">notes</Link>
+          <Link style={padding} to="/users">users</Link>
+          {/*Information about the logged in user is displayed, and login view is displayed conditionally */}
+          {user
+            ? <em>{user} logged in</em>
+            : <Link style={padding} to="/login">login</Link>
+          }
+        </div>
+
+        <Routes>
+          {/* notes/:id is defined as a parameterized URL */}
+          <Route path="/notes/:id" element={<Note note={note} />} />
+          <Route path="/notes" element={<Notes notes={notes} />} />
+          {/* If a user isn't logged in, redirect to /login if they try to click Users */}
+          <Route path="/users" element={user ? <Users /> : <Navigate replace to="/login" />} />
+          <Route path="/login" element={<Login onLogin={login} />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      <footer>
+        <br />
+        <em>Note app, Department of Computer Science 2023</em>
+      </footer>
+    </div>
+  )
+}
+
+export default App
