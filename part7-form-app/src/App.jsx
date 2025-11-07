956ed8ec6445c1e9b0e40340c5b735f842ddcd37
Sarah Emmett <sarah.welton@couchbase.com>
2025-11-07 12:15:33 -0500

    Notes from Part 7b

---
+++ part7-form-app/src/App.jsx
@@ -0,0 +1,82 @@
+import { useState } from 'react'
+
+// Then, we define a custom useField hook to simplify the state management
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+// The content of the app becomes much simpler. 
+const App = () => {
+  const name = useField('text')
+  const born = useField('date')
+  const height = useField('number')
+
+  return (
+    <div>
+      <form>
+        name: 
+        <input  {...name} /> 
+        <br/> 
+        birthdate:
+        <input {...born} />
+        <br /> 
+        height:
+        <input {...height} />
+      </form>
+      <div>
+        {name.value} {born.value} {height.value}
+      </div>
+    </div>
+  )
+}
+
+// In the first iteration, we just have separate states for each form field
+/*
+const App = () => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+  const [height, setHeight] = useState('')
+
+  return (
+    <div>
+      <form>
+        name: 
+        <input
+          type='text'
+          value={name}
+          onChange={(event) => setName(event.target.value)} 
+        /> 
+        <br/> 
+        birthdate:
+        <input
+          type='date'
+          value={born}
+          onChange={(event) => setBorn(event.target.value)}
+        />
+        <br /> 
+        height:
+        <input
+          type='number'
+          value={height}
+          onChange={(event) => setHeight(event.target.value)}
+        />
+      </form>
+      <div>
+        {name} {born} {height} 
+      </div>
+    </div>
+  )
+}
+*/
+
+export default App
