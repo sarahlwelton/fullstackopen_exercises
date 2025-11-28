140567e757f4365da52e302bbbe441022d94af99
Sarah Emmett <sarah.welton@couchbase.com>
2025-11-28 15:54:11 -0500

    Notes from Part 7c - convert app to use Bootstrap

--- part7-react-router-notes/src/App.jsx
+++ part7-react-router-notes/src/App.jsx
@@ -9,7 +9,7 @@ import {
   useNavigate,
   useMatch
 } from "react-router-dom"
-
+import { Table, Form, Button, Alert, Navbar, Nav } from 'react-bootstrap'
 
 const Home = () => (
   <div>
@@ -32,16 +32,26 @@ const Note = ({ note }) => {
   )
 }
 // We render Notes as a clickable Link component that takes you to the body of the note
+// With Bootstrap, we can also render it as a table
 const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
-    <ul>
-      {notes.map(note =>
-        <li key={note.id}>
-          <Link to={`/notes/${note.id}`}>{note.content}</Link>
-        </li>
+    <Table striped>
+      <tbody>
+        {notes.map(note =>
+        <tr key={note.id}>
+          <td>
+            <Link to={`/notes/${note.id}`}>
+              {note.content}
+            </Link>
+          </td>
+          <td>
+            {note.user}
+          </td>
+        </tr>
       )}
-    </ul>
+      </tbody>
+    </Table>
   </div>
 )
 
@@ -70,15 +80,22 @@ const Login = (props) => {
   return (
     <div>
       <h2>login</h2>
-      <form onSubmit={onSubmit}>
-        <div>
-          username: <input />
-        </div>
-        <div>
-          password: <input type='password' />
-        </div>
-        <button type="submit">login</button>
-      </form>
+      <Form onSubmit={onSubmit}>
+        <Form.Group>
+          <Form.Label>username:</Form.Label>
+          <Form.Control
+            type="text"
+            name="username"
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>password:</Form.Label>
+          <Form.Control
+            type="password"
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">login</Button>
+      </Form>
     </div>
   )
 }
@@ -106,9 +123,14 @@ const App = () => {
   ])
 
   const [user, setUser] = useState(null)
+  const [message, setMessage] = useState(null)
 
   const login = (user) => {
     setUser(user)
+    setMessage(`Welcome ${user}`)
+    setTimeout(() => {
+      setMessage(null)
+    }, 10000)
   }
 
   const padding = {
@@ -121,20 +143,35 @@ const App = () => {
   const note = match 
     ? notes.find(note => note.id === Number(match.params.id))
     : null
-
+  // We add the "container" className to start using Bootstrap
   return (
-    <div>
-        <div>
-          <Link style={padding} to="/">home</Link>
-          <Link style={padding} to="/notes">notes</Link>
-          <Link style={padding} to="/users">users</Link>
-          {/*Information about the logged in user is displayed, and login view is displayed conditionally */}
-          {user
-            ? <em>{user} logged in</em>
-            : <Link style={padding} to="/login">login</Link>
-          }
-        </div>
-
+    <div className="container">
+      {(message &&
+        <Alert variant="success">
+          {message}
+        </Alert>)}
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#" as="span">
+                <Link style={padding} to="/">home</Link>
+              </Nav.Link>
+              <Nav.Link href="#" as="span">
+                <Link style={padding} to="/notes">notes</Link>
+              </Nav.Link>
+              <Nav.Link href="#" as="span">
+                <Link style={padding} to="/users">users</Link>
+              </Nav.Link>
+              <Nav.Link href="#" as="span">
+                {user
+                  ? <em style={padding}>{user} logged in</em>
+                  : <Link style={padding} to="/login">login</Link>
+                }
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
         <Routes>
           {/* notes/:id is defined as a parameterized URL */}
           <Route path="/notes/:id" element={<Note note={note} />} />
